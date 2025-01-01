9979191815423c6450290264d23c7043647a05ec
John Doe <reconstructed.philo@gmail.com>
2025-01-01 15:49:27 +0200

    m2bool

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import cat from "../public/laughing-cat.png"
+import cat from "../public/laughing-cat.jpg"
 const Home = () => {
     return (
         <div className='w-screen h-screen flex justify-center items-center absolute z-0 top-0'>
             <img className='w-screen h-screen' src={cat} alt=""/>
-            <h1 className='w-screen absolute text-center text-5xl te py-4'>GET PURPULED NIGGA</h1>
         </div>
     )
 }
--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,8 +3,9 @@ import gojo from "../public/theGoat.webp"
 const Loader = () => {
 
   return (
-    <div className="flex justify-center items-center overflow-hidden h-svh z-10">
-        <div className='w-screen h-screen flex loader-parent justify-center items-center z-10'>
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <div className="flex w-screen h-svh sm:h-[300px] justify-center items-center overflow-hidden z-10">
+        <div className='w-full h-full flex loader-parent justify-center items-center z-10'>
           <div className='bg-[#c1c1c1] h-full w-full'></div>
           <div className='bg-[#c1c100] h-full w-full'></div>
           <div className='bg-[#00c1c1] h-full w-full'></div>
@@ -13,7 +14,8 @@ const Loader = () => {
           <div className='bg-[#c10000] h-full w-full'></div>
           <div className='bg-[#0000c1] h-full w-full'></div>
         </div>
-        <img src={gojo} alt="" className='absolute w-[100%] opacity-0 thegoat'/>
+        <img src={gojo} alt="" className='absolute w-[100%] opacity-0 thegoat' />
+      </div>
     </div>
   )
 }
